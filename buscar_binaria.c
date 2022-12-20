76db9e7e3c464c03bf846cc72d0b883887de4969
educhagas00 <111936750+educhagas00@users.noreply.github.com>
2022-12-20 00:51:14 -0300

    Add files via upload

---
+++ buscar_binaria.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int busca(int* vet, int inicio, int fim, int numero);
+
+int main()
+{
+    int n, x;
+    printf("tamanho do vetor:\n");
+    scanf("%d", &n);
+    
+    int vet[n];
+    
+    
+    
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &vet[i]);
+    }
+    
+    printf("qual numero quer buscar?\n");
+    scanf("%d", &x);
+    
+    int k = busca(vet, 0, n, x);
+
+    printf("esta na posicao : %d", k);
+
+    return 0;
+}
+
+int busca(int* vet, int inicio, int fim, int numero)
+{
+
+    if(inicio <= fim)
+    {
+        int meio = (inicio + fim)/2;
+
+        if(vet[meio] == numero)
+        {
+            return meio;
+        }
+        if(numero < vet[meio])
+        {
+            return busca(vet , inicio , meio-1 , numero);
+        }
+        else
+        {
+            return busca(vet , meio+1 , fim , numero);
+        }
+    }
+    return -1;
+}
